3f864cd82aa4fd33ada1937cb0f023cccda96217
Siphiwe Mohale <phiwesj90@hotmail.com>
2020-08-25 09:08:08 +0200

    first commit

---
+++ src/Heading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Heading() {
+    return (
+        <div>
+            <h1>FIND THE LATEST CURRENCY EXCHANGE RATES</h1> 
+        </div>
+    )
+}
