6a762d53200536332fd1af5eb77a9a2d6dd43a79
nicholas harper <nicholasharper000@gmail.com>
2024-12-23 01:42:40 -0800

    Update Heading.js

--- src/Heading.js
+++ src/Heading.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export default function Heading() {
+export default function Heading() 
+{
     return (
         <div>
             <h1>FIND THE LATEST CURRENCY EXCHANGE RATES</h1> 
