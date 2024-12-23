0f73417eb1cc7274663d0113c509e50a50d64dfc
nicholas harper <nicholasharper000@gmail.com>
2024-12-23 05:34:10 -0800

    Update CurrencyRow.js

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function CurrencyRow(props) {
-    const {
+export default function CurrencyRow(props) 
+{
+    const 
+    {
         currencyOptions,
         selectedCurrency,
         onChangeCurrency,
